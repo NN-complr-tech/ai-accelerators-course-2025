b2a34b857b407fcca8b302caed199aefd75c180d
MypkaXD <nicklayschannel@yandex.ru>
2025-11-21 00:11:41 +0300

    lab3 alpha (it's working Johnny!)

--- tasks/03-matmul-cuda/chuvashev/src/kernel.cu
+++ tasks/03-matmul-cuda/chuvashev/src/kernel.cu
@@ -1,6 +1,7 @@
 #include <cuda.h>
 #include <cuda_fp16.h>
 #include <cuda_runtime.h>
+#include <mma.h>
 #include <omp.h>
 
 #include <algorithm>
@@ -17,10 +18,11 @@
 #include <string>
 #include <vector>
 
-#include <mma.h>
+#include <cutlass/gemm/device/gemm.h>
 
 #define WARP_SIZE 32
 #define WMMA_SIZE 16
+using namespace nvcuda;
 
 #define BLOCK_SIZE 32
 
@@ -41,7 +43,6 @@ double timer::elapsed() {
              std::chrono::high_resolution_clock::now() - t_start)
       .count();
 }
-
 
 // TODO: Move to utils
 #define CHECK_CUDA_ERROR(callable)                                        \
@@ -171,8 +172,15 @@ void run_matrix_mult_gpu_ver_2(const std::vector<__half> &input,
 
   timer timer;
   GPU_MATMUL_V2<<<grid_size, block_size>>>(d_input, d_output, n);
-  double end_time = timer.elapsed(); 
-  std::cout << end_time << "\t" << "GB\s: " << n * n * (2.0f * sizeof(__half) + 1.0f*sizeof(float))  / (1024.0f * 1024.0f * 1024.0f * end_time) << std::endl;
+  cudaDeviceSynchronize();
+  double end_time = timer.elapsed();
+  std::cout << end_time << "\t"
+            << "GB\s: "
+            << float(n * n * sizeof(float) +
+                     n * n * float((n + block_size.x - 1) / block_size.x) * 2 *
+                         sizeof(__half)) /
+                   (1024.0f * 1024.0f * 1024.0f * end_time)
+            << std::endl;
 
   cudaMemcpy(output.data(), d_output, sizeof(float) * n * n,
              cudaMemcpyDeviceToHost);
@@ -185,20 +193,121 @@ void warmup_wmma(const std::vector<__half> &matrix, std::size_t n) {
   throw std::runtime_error("WMMA warm-up not implemented");
 }
 
-void run_wmma(const std::vector<__half> &intput, std::vector<float> &output,
+__global__ void WMMA_kernel(const __half *input, float *output,
+                            const std::size_t n) {
+  int warp_i = (blockIdx.x * blockDim.x + threadIdx.x) / warpSize;
+  int warp_j = (blockIdx.y * blockDim.y + threadIdx.y);
+
+  wmma::fragment<wmma::matrix_a, WMMA_SIZE, WMMA_SIZE, WMMA_SIZE, __half,
+                 wmma::row_major>
+      a_frag;
+  wmma::fragment<wmma::matrix_b, WMMA_SIZE, WMMA_SIZE, WMMA_SIZE, __half,
+                 wmma::row_major>
+      b_frag;
+  wmma::fragment<wmma::accumulator, WMMA_SIZE, WMMA_SIZE, WMMA_SIZE, float>
+      acc_frag;
+
+  wmma::fill_fragment(acc_frag, 0.0f);
+
+  for (std::size_t k = 0; k < n; k += WMMA_SIZE) {
+    int a_row = warp_i * WMMA_SIZE;
+    int a_col = k;
+    int b_row = k;
+    int b_col = warp_j * WMMA_SIZE;
+
+    wmma::load_matrix_sync(a_frag, input + a_row * n + a_col, n);
+    wmma::load_matrix_sync(b_frag, input + b_row * n + b_col, n);
+
+    wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
+  }
+
+  int c_row = warp_i * WMMA_SIZE;
+  int c_col = warp_j * WMMA_SIZE;
+
+  wmma::store_matrix_sync(output + c_row * n + c_col, acc_frag, n,
+                          wmma::mem_row_major);
+}
+
+void run_wmma(const std::vector<__half> &input, std::vector<float> &output,
               const std::size_t n) {
   // throw std::runtime_error("WMMA method not implemented");
 
-  
+  __half *d_input;
+  float *d_output;
+
+  cudaMalloc(&d_input, n * n * sizeof(__half));
+  cudaMalloc(&d_output, n * n * sizeof(float));
+
+  cudaMemcpy(d_input, input.data(), n * n * sizeof(__half),
+             cudaMemcpyHostToDevice);
+
+  dim3 block_size(4 * WARP_SIZE, 4);
+  dim3 block_count((n / block_size.x) * (WARP_SIZE / WMMA_SIZE),
+                   (n / block_size.y) / WMMA_SIZE);
+  timer timer;
+  WMMA_kernel<<<block_count, block_size>>>(d_input, d_output, n);
+  cudaDeviceSynchronize();
+  double time = timer.elapsed();
+  std::cout << "Time: " << time << std::endl;
+
+  cudaMemcpy(output.data(), d_output, n * n * sizeof(float),
+             cudaMemcpyDeviceToHost);
+
+  cudaFree(d_input);
+  cudaFree(d_output);
 }
 
 void warmup_cutlass(const std::vector<__half> &matrix, std::size_t n) {
   throw std::runtime_error("CUTLASS warm-up not implemented");
 }
 
-std::vector<float> run_cutlass(const std::vector<__half> &matrix,
-                               std::size_t n) {
-  throw std::runtime_error("CUTLASS method not implemented");
+cudaError_t CutlassGEMM(const cutlass::half_t *input, float *output,
+                        const std::size_t n) {
+  using RowMajor = cutlass::layout::RowMajor;
+  using OpClassTesorOp = cutlass::arch::OpClassTensorOp;
+  using Sm75 = cutlass::arch::Sm75;
+  using CutlassGemm =
+      cutlass::gemm::device::Gemm<cutlass::half_t, RowMajor,
+                                  cutlass::half_t, RowMajor, float, RowMajor,
+                                  float, OpClassTesorOp, Sm75>;
+
+  CutlassGemm::Arguments args({cutlass::gemm::GemmCoord::Index(n), cutlass::gemm::GemmCoord::Index(n),
+       cutlass::gemm::GemmCoord::Index(n)},
+      {input, n}, {input, n}, {output, n},
+                              {output, n}, {1, 0});
+
+  CutlassGemm gemm_operator;
+  cutlass::Status status = gemm_operator(args);
+  return status == cutlass::Status::kSuccess ? cudaSuccess : cudaErrorUnknown;
+}
+
+void run_cutlass(const std::vector<__half> &input, std::vector<float> &output,
+                 std::size_t n) {
+  // throw std::runtime_error("CUTLASS method not implemented");
+
+  cutlass::half_t *d_input;
+  float *d_output;
+
+  cudaMalloc(&d_input, n * n * sizeof(__half));
+  cudaMalloc(&d_output, n * n * sizeof(float));
+
+  cudaMemcpy(d_input, input.data(), n * n * sizeof(__half),
+             cudaMemcpyHostToDevice);
+
+  cudaError_t status = CutlassGEMM(d_input, d_output, n);
+  cudaDeviceSynchronize();
+
+  timer timer;
+  status = CutlassGEMM(d_input, d_output, n);
+  cudaDeviceSynchronize();
+  double time = timer.elapsed();
+  std::cout << "GEMM Time: " << time << std::endl;
+
+  cudaMemcpy(output.data(), d_output, n * n * sizeof(float),
+             cudaMemcpyDeviceToHost);
+
+  cudaFree(d_input);
+  cudaFree(d_output);
 }
 
 double measure_seconds(const std::function<void()> &work) {
@@ -263,7 +372,7 @@ int main(int argc, char *argv[]) {
       throw std::invalid_argument("Matrix size must be positive");
     }*/
 
-    std::size_t n = 4096;
+    std::size_t n = 1024;
     std::vector<__half> input(n * n, 0);
     make_input_matrix(input, n);
     // print_matrix<__half>(input, n);
@@ -309,31 +418,33 @@ int main(int argc, char *argv[]) {
       print_report("MMGV2", mmgv2_res);
       // print_matrix<float>(mmgv1_res.result, n);
     }
-
-    /*RunResult wmma_res;
-    try {
-      warmup_wmma(input, n);
-      wmma_res.seconds = measure_seconds([&]() { return run_wmma(input, n); },
-                                         wmma_res.result);
-      wmma_res.diff = max_abs_diff(openmp_result, wmma_res.result);
-      wmma_res.success = true;
-    } catch (const std::exception &ex) {
-      std::cerr << "WMMA method failed: " << ex.what() << '\n';
+    {
+      RunResult wmma_res;
+      wmma_res.result.resize(n * n, 0);
+      try {
+        // warmup_wmma(input, n);
+        wmma_res.seconds = measure_seconds(
+            [&]() { return run_wmma(input, wmma_res.result, n); });
+        wmma_res.diff = max_abs_diff(openmp_result, wmma_res.result);
+        wmma_res.success = true;
+      } catch (const std::exception &ex) {
+        std::cerr << "WMMA method failed: " << ex.what() << '\n';
+      }
+      print_report("WMMA", wmma_res);
     }
-
-    RunResult cutlass_res;
-    try {
-      warmup_cutlass(input, n);
-      cutlass_res.seconds = measure_seconds(
-          [&]() { return run_cutlass(input, n); }, cutlass_res.result);
-      cutlass_res.diff = max_abs_diff(openmp_result, cutlass_res.result);
-      cutlass_res.success = true;
-    } catch (const std::exception &ex) {
-      std::cerr << "CUTLASS method failed: " << ex.what() << '\n';
-    }*/
-
-    /*print_report("WMMA", wmma_res);
-    print_report("CUTLASS", cutlass_res);*/
+    {
+      RunResult cutlass_res;
+      cutlass_res.result.resize(n * n, 0);
+      try {
+        cutlass_res.seconds = measure_seconds(
+            [&]() { return run_cutlass(input, cutlass_res.result, n); });
+        cutlass_res.diff = max_abs_diff(openmp_result, cutlass_res.result);
+        cutlass_res.success = true;
+      } catch (const std::exception &ex) {
+        std::cerr << "CUTLASS method failed: " << ex.what() << '\n';
+      }
+      print_report("CUTLASS", cutlass_res);
+    }
 
     return EXIT_SUCCESS;
   } catch (const std::exception &ex) {
