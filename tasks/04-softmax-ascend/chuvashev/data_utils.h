048045be233f560e01375728ddca1a43112ca57c
Chuvashev_AA <tchuwashev2016@yandex.ru>
2025-12-17 08:02:22 +0000

    start working with ascend

---
+++ tasks/04-softmax-ascend/chuvashev/data_utils.h
@@ -0,0 +1,13 @@
+/**
+ * @file data_utils.h
+ *
+ * Copyright (C) 2024. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+#ifndef DATA_UTILS_H
+#define DATA_UTILS_H
+
+#endif  // DATA_UTILS_H
